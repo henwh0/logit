332610c35e9f6eb58b2f30d54287030f11a19012
henwh0 <hmcginnis96@gmail.com>
2026-02-24 22:31:09 -0500

    Update logit.sh
    ancillary changes.

--- logit.sh
+++ logit.sh
@@ -5,19 +5,25 @@ PID=$$
 DEFAULT_PROGRAM_NAME=$(basename "$0")
 
 logit() {
-  local PROGRAM_NAME
-  local MESSAGE
+    local PROGRAM_NAME
+    local MESSAGE
 
-  if [[ $# -eq 1 ]]; then
-    PROGRAM_NAME=$DEFAULT_PROGRAM_NAME
-    MESSAGE="$1"
-  else
-    PROGRAM_NAME=$1
-    shift
-    MESSAGE=$2
-  fi
+    if [[ $# -eq 0 ]]; then
+        echo "Usage: logit [program_name] message" >&2
+        return 1
+    fi
 
-  local TIMESTAMP=$(date +"%Y/%m/%d %H:%M:%S")
+    if [[ $# -eq 1 ]]; then
+        # Only message provided, use default program name
+        PROGRAM_NAME=$DEFAULT_PROGRAM_NAME
+        MESSAGE="$1"
+    else
+        # Program name and message provided
+        PROGRAM_NAME=$1
+        shift
+        MESSAGE="$*"
+    fi
 
-  printf '%b\n' "${TIMESTAMP} - ${HOST} - ${PROGRAM_NAME}[${PID}]: ${MESSAGE}"
+    local TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "${TIMESTAMP} ${HOST} ${PROGRAM_NAME}[${PID}]: ${MESSAGE}" | tee -a "$LOG_FILE"
 }
